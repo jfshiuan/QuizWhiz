81faa9673772dbfd770e9cd72b1aab0c29fcabaf
Jason Shiuan <jfshiuan@ucsd.edu>
2016-01-28 17:14:18 -0800

    a4 submission

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,13 +8,15 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("See you next time!");
-		$("#testjs").text("Loging out");
-		$(".jumbotron p").toggleClass("active");
-		$("#submitBtn").click(updateProject); 
-	});
+ function initializePage() {
+ 	$("#testjs").click(function(e) {
+ 		$('.jumbotron h1').text("See you next time!");
+ 		$("#testjs").text("Login");
+ 		$(".jumbotron p").toggleClass("active");
+ 		$("#courseBtn1").click(updateProject);
+ 		$("#courseBtn2").click(updateProject);
+ 		$("#playBtn2").click(updateProject);
+ 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
@@ -33,23 +35,22 @@ function projectClick(e){
 
 	/*var containingProject = $(this).closest(".project");
     containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-	*/
+    */
 
-	var containingProject = $(this).closest(".project");
+    var containingProject = $(this).closest(".project");
     var description = $(containingProject).find(".project-description");
     if (description.length == 0) {
-       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    	description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
     }
 }
 
 function updateProject(e) {
-   var projectID = $('#project').val();
-   $(projectID).animate({
-      width: $('#width').val()
-   });
-
-   var newText = $('#description').val();
-   $(projectID + " .project-description").text(newText);
+	var projectID = $('#project').val();
+	$(projectID).animate({
+		width: $('#width').val()
+	});
+	var newText = $('#description').val();
+	$(projectID + " .project-description").text(newText);
 }
